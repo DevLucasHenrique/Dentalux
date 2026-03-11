25e95e2c5647e6b569999e8c3a6fc2be0b371284
DevLucasHenrique <lucashenriquecalixto355@gmail.com>
2026-03-11 19:40:25 -0300

    Enables routing and updates branding assets
    Finish Header
    
    Integrates router support to prepare for multi-page navigation.
    Updates favicon and app title for clearer product identity.
    Removes unused styling and adds icon/font dependencies for UI enhancements.
    Allows JavaScript in TS config for flexibility.

---
+++ dentalux-app/src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import logoMobile from "../../assets/images/logoBlue.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import './Header.css'
+
+export function Header() {
+  return (
+    <header>
+      <nav id="mobile-nav">
+        <img src={logoMobile} className="logo-mobile" alt="Logo" />
+        <div className="end-nav">
+          <button className="mobile-services">Ver especialidades</button>
+          <button className="mobile-menu-button">
+            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
